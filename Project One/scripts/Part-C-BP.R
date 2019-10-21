6e879eca205881dd72b297902e639def6d9222b7
Bethany Poulin <bpoulin@SC-PoulinB.local>
2019-10-20 21:59:46 -0400

    updating final script

--- Project One/scripts/Part-C-BP.R
+++ Project One/scripts/Part-C-BP.R
@@ -7,17 +7,17 @@ require(lubridate)
 require(psych)
 require(xlsx)
 options(scipen = 999)
-file ='data/water-pipes.xlsx'
-# 10-23-15 at 1:00AM is hour 7081 Pipe 1
-# End Date Hour is hour 7319
-#10-23-15 00:00 AM is hour 7080
-# End Date Hour 12-3-2015 16:00:00 is 8080
+
+# Reading Data
 waterflow_1 <- read_excel("data/Waterflow_Pipe1.xlsx")
 waterflow_2 <- read_excel("data/Waterflow_Pipe2.xlsx")
 
-#write.xlsx(waterflow_1, file =  file , sheetName ="Waterflow Pipe 1", col.names = TRUE, row.names = TRUE, append = FALSE)
+# Writing original data to submission file
+file ='forecasts/water-pipes.xlsx'
+write.xlsx(waterflow_1, file =  file , sheetName ="Waterflow Pipe 1", col.names = TRUE, row.names = TRUE, append = FALSE)
+write.xlsx(waterflow_2, file=file, sheetName = "Waterflow Pipe 2", col.names = TRUE, row.names = TRUE, append = TRUE)
 
-#write.xlsx(waterflow_2, file=file, sheetName = "Waterflow Pipe 2", col.names = TRUE, row.names = TRUE, append = TRUE)
+# Grooming, aligning dates and aggregating Data
 waterflow_1<-waterflow_1 %>% 
     mutate(DateTime = as.POSIXct(DateTime))%>%
     group_by(hour=floor_date(DateTime, "hour")) %>%
@@ -29,31 +29,18 @@ waterflow_2<-waterflow_2 %>%
     group_by(hour=floor_date(DateTime, "hour")) %>%
     summarize(WaterFlow=mean(WaterFlow))
 
+# Creating a combined data set
 waterflow_all <-merge(waterflow_1, waterflow_2, by = 'hour', all = TRUE)%>%
     mutate(waterflow = rowSums(.[c("WaterFlow.y", "WaterFlow.x")], na.rm = TRUE))%>%
     select(hour, waterflow)
 
-
+# Converting all Three Data Sets to Time Series
 w1<-ts(waterflow_1$WaterFlow ,start=c(1,7081),frequency=24)
 w2<-ts(waterflow_2$WaterFlow ,start=c(1,7081),frequency=24)
 ws <- ts(waterflow_all$waterflow ,start=c(1,7081),frequency=24)
-#shows calendar form of 24 hour periods for days between 10-23-2015 & 12-3-2015
-#print(ts(waterflow_all$waterflow, frequency = 24, start = c(1, 7081)), calendar = T)
 
 
-# ws%>%
-#     autoplot()+
-#     labs(title = 'Flow Rates Combined Pipes (pipe one stops on November 1, 2015)',
-#          subtitle ='October 23, 2015 00:00 - December 3, 2015 00:00',
-#          x = 'Day of Year')
-# 
-# ws%>%
-#     decompose()%>%
-#     autoplot()+
-#     labs(x = 'Day of Year')
-
-
-
+#Decomposition of Time Series
 ws_decomp<- ws%>% 
     decompose()%>%
     autoplot()+
@@ -63,12 +50,12 @@ ws_decomp<- ws%>%
     theme_bw()
 
 
-
+# Checking Differences
 ws_diffs<- ws%>%
-    ndiffs()
-#differences: 1
+    ndiffs() #1
 
 
+# Testing Stationarity
 dickie<-tseries::adf.test(ws)
 
 # ACF & PACF
@@ -85,7 +72,7 @@ ws_pacf <- ggPacf(ws, color = 'darkorange4')+
          y="Partial Auto Correlation", x="Hours")+
     theme_bw()+ theme()
 
-# Diffed ACF
+# Differencesd ACF & PACF
 
 ws_acf_diff <-ggAcf(diff(ws,lag = 1), color = 'darkorange4')+
     labs(title = "ACF Combined Pipe Flow Rates", 
@@ -99,13 +86,18 @@ ws_pacf_diff <-ggPacf(diff(ws,lag = 1), color = 'darkorange4')+
          y="Auto Correlation", x="Hours")+
     theme_bw()+ theme()
 
+#Establishing a lambda value for ARIMA transformations
 lambda <-  BoxCox.lambda(ws)
 #Lambda = 0.9531552
 
+
+# Auto arima's including season components for AICc and BIC
 aic<- auto.arima(ws, seasonal = TRUE, ic = 'aicc', lambda = lambda)
 
+bic<-auto.arima(ws, seasonal = TRUE, ic = 'bic', lambda = lambda )
 
 
+# Plots of auto.arimas
 aic_plot <- auto.arima(ws, seasonal = TRUE, ic = 'aicc', lambda = lambda)%>%
     forecast(h=24*7)%>%
     autoplot() +
@@ -113,8 +105,6 @@ aic_plot <- auto.arima(ws, seasonal = TRUE, ic = 'aicc', lambda = lambda)%>%
                  subtitle = 'October 23, 2015 - December 23, 2015',
                 y="Flowrate", x="Days")+
     theme_bw()+ theme()
-
-bic<-auto.arima(ws, seasonal = TRUE, ic = 'bic', lambda = lambda )
 
     
 bic_plot<-auto.arima(ws, seasonal = TRUE, ic = 'bic', lambda = lambda )%>%
@@ -125,24 +115,26 @@ bic_plot<-auto.arima(ws, seasonal = TRUE, ic = 'bic', lambda = lambda )%>%
          y="Flowrate", x="Days")+
     theme_bw()+ theme()
 
+
+# Final AIC from AICc and predictions
 final_ws <- Arima(ws, order=c(1,1,3), seasonal=c(0,0,1),lambda=lambda)
 
 preds_ws <-as.data.frame(forecast(final_ws, h = 168))
 
 
-
+#Renaming fields for output data
 waterflow_all <-waterflow_all%>%
     rename( DateTime = hour,
             WaterFlow = waterflow)
-    
+# Formatting forecasts for output data    
 preds_ws<-preds_ws%>%
     mutate(DateTime = seq(from=as.POSIXct("2015-12-3 17:00", tz="UTC"),
                           to=as.POSIXct("2015-12-10 16:00", tz="UTC"), 
                           by="hour") )%>%
     select(DateTime, `Point Forecast`, `Lo 80`,`Hi 80`, `Lo 95`, `Hi 95`)
 
-
-#write.xlsx(waterflow_all, file = file, sheetName = "Combined Waterflow", col.names = TRUE, row.names = FALSE, append = TRUE)
-#write.xlsx(preds_ws, file =  file , sheetName = "Forecasts", col.names = TRUE, row.names = FALSE, append = TRUE)
+# Writing forecasts and final data to the 'XLSX' file
+write.xlsx(waterflow_all, file = file, sheetName = "Combined Waterflow", col.names = TRUE, row.names = FALSE, append = TRUE)
+write.xlsx(preds_ws, file =  file , sheetName = "Forecasts", col.names = TRUE, row.names = FALSE, append = TRUE)
 
 
