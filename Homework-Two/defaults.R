a09ce10e8552c7e4ca46c07a4e8c7a2b206b18eb
jemceach <41031366+jemceach@users.noreply.github.com>
2019-11-25 01:36:01 -0500

    Updated merge of HW2-1

--- Homework-Two/defaults.R
+++ Homework-Two/defaults.R
@@ -7,20 +7,21 @@ library(easypackages)
 library(formatR)
 
 # Set default augments for code chunks
-knitr::opts_chunk$set(echo = F, message=F, warning=F, error=F, comment=NA,self.contained = F, tidy=T, tidy.opts=list(width.cutoff=60), fig.width=10, fig.height = 3)
+knitr::opts_chunk$set(echo = F, message=F, warning=F, error=F, comment=NA, tidy=T, tidy.opts=list(width.cutoff=60), fig.width=10, fig.height = 3)
 
 # Set default augments for `kable()` 
-default(kable) <- list(format="latex")
+default(kable) <- list(format="latex", digits=4)
 
 # Set default augments for `kable_styling()` 
-default(kable_styling)  <- list(latex_options = c("HOLD_position", "striped"))
+default(kable_styling)  <- list(latex_options = c("HOLD_position", "striped"), font_size=8)
 default(row_spec) <- list(row=0, bold=T)
 
 # Set default augments for ggplot2 `theme()`
-default(theme) <- list(axis.text.x = element_text(angle = 0, hjust = NULL),
-                       plot.title = element_text(color="#2d8659", size=12, face="bold"),
+default(theme) <- list(axis.title.x = element_text(color="#7d7464", face="bold", size=10, angle = 0, hjust = NULL),
+                       axis.title.y = element_text(color="#7d7464", face="bold", size=10),
+                       plot.title = element_text(color="#745010", size=12, face="bold"),
                        plot.subtitle = (element_text(size=10, color="#868b8c")),
-                       legend.title = (element_text(size=10, color="#2d8659", face="bold")),
+                       legend.title = (element_text(size=10, color="#745010", face="bold")),
                        strip.background = element_rect(color="#000000", 
-                                                       fill="#d9f2e6", size=.75,linetype="solid"),
+                                                       fill="#eee9e1", size=.75,linetype="solid"),
                        strip.text.x = element_text(size = 8, color = "#000000", face="bold"))
